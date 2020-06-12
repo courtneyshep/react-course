a16526ba1d55c0c8d9c726b2751f9c339391f84b
“courtneyshep” <courtneyjshepherd@gmail.com>
2020-06-12 13:59:31 +0100

    Updates

---
+++ precache-manifest.db827e878e096db547b631793d3bf5ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74587bc58870c3a155ab0ef9c8c0ee12",
+    "url": "/react-course/index.html"
+  },
+  {
+    "revision": "02fb0f2bfa29d7d4a01e",
+    "url": "/react-course/static/css/main.7dcae93e.chunk.css"
+  },
+  {
+    "revision": "cb80a15f71da0f888782",
+    "url": "/react-course/static/js/2.668f8c51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course/static/js/2.668f8c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02fb0f2bfa29d7d4a01e",
+    "url": "/react-course/static/js/main.488be281.chunk.js"
+  },
+  {
+    "revision": "5d4a7062a2ddb18bf77a",
+    "url": "/react-course/static/js/runtime-main.77b0c8b5.js"
+  }
+]);
